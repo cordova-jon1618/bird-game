1d2cead5c697b1086f0c77dde9a3b27e7dc0ea6c
Jon <cordovaj63@gmail.com>
2023-01-31 10:10:36 -0800

    Added game over and restart game functionality

--- bird-game/Assets/BirdScript.cs
+++ bird-game/Assets/BirdScript.cs
@@ -6,16 +6,19 @@ public class BirdScript : MonoBehaviour
 {
     public float flapStrength = 5;
     public Rigidbody2D myRigidbody;
+    public bool birdIsAlive = true;
+    public LogicScript logic;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Space) == true)
+        if(Input.GetKey(KeyCode.Space) == true && birdIsAlive == true)
         {
             myRigidbody.velocity = Vector2.up * flapStrength;
         }
@@ -24,7 +27,11 @@ public class BirdScript : MonoBehaviour
         {
             Application.Quit();
         }
-
-
     }//end Update
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
 }//end class
